b60d926fd3ac2c301a1339e0cb61ec2f7340a85c
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-10 16:49:51 +0100

    Add files via upload

---
+++ A3.py
@@ -0,0 +1,9 @@
+# Creating differen Variables
+x = 5
+y = "Shreeyansh"
+print(x)
+print(y)
+codingal = "Shreeyansh"
+print(codingal)
+name=input("Enter Your Name")
+print("\n Hello",name,"Welcome to codingal")
