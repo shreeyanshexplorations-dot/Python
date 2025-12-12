913ebd7b35697e76b57a02c85ca2879f94f1cd37
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-12 17:53:45 +0100

    Add files via upload

---
+++ L3A2.py
@@ -0,0 +1,19 @@
+Name = "Shreeyansh"
+Age = "9"
+is_student= True
+Weight = "25.7"
+print("name :",Name)
+print("Data Type Of Name Is",type(Name))
+print("Age :",Age)
+print("Data Type Of Age Is",type(Age))
+print("is_student :",is_student)
+print("Data Type Of is-student Is",type(is_student))
+print("Weight :",Weight)
+print("Data Type Of Weight Is",type(Weight))
+print("\n After Type Casting....")
+Age = str(Age)
+print(Age)
+print("Data Type Of Age Is",type(Age))
+Weight=int(Weight)
+print(weight)
+print("Data Type Of Weight Is", type(Weight))
