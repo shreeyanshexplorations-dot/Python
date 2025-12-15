0d4b9fb473a07b8b4d8b3dcd1c385e7e8eb29a5f
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-15 16:44:24 +0100

    Add files via upload

---
+++ L4A3.py
@@ -0,0 +1,10 @@
+print("Enter Mark Obtained In 4 Subjects:")
+maths = int(input("maths:"))
+english = int(input("english:"))
+science = int(input("science:"))
+hindi = int(input("hindi:"))
+sum = maths+english+science+hindi
+print("Sun Of math,english,science and hindi")
+perc = (sum/400)*100
+print(end="Percentage Mark=")
+print(perc)
