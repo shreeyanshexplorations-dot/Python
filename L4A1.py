9a8f5a8fc6519661f72a7a5c96948d883cc976b0
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-15 16:18:36 +0100

    Add files via upload

---
+++ L4A1.py
@@ -0,0 +1,10 @@
+
+tree1= 104
+tree2= 56
+tree3= 67
+tree4= 98
+tree5= 10
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all the 5 trees is:",sum)
+average = sum/5
+print("The average of all the tree is:", average)
