8302ae9159d7a90faef851cde3c1bb027a0e22b9
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-22 16:43:53 +0100

    Add files via upload

---
+++ L6A1.py
@@ -0,0 +1,20 @@
+a = 93
+b = 67
+c = 0
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as false")
+a = 17
+b = -25
+c = 0
+if a > 0 or b > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+
+if b > 0 or c > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
