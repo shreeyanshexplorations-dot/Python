b7a58081fdf8aef2a8192477b0d4b9756f653af4
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2025-12-26 16:21:02 +0100

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,6 @@
+
+ch = input("Enter a character:")
+if ('a' <= ch <='z') or ('A' <= ch <='Z'):
+  print("The character is an alphabet")
+else:
+   print("The character is not an alphabet")
---
+++ Conditional statements.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter the current temperature in °C "))
+if temperature >= 20 and temperature <= 30:
+    print("The weather is suitable for light clothes.")
+elif temperature < 20:
+    print("Its to cold, wear a jacket.")
+else:
+    print("The weather is suitable for light clothes.")
+
