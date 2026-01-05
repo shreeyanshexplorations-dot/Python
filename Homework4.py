7a612d2ba49ff1c7cbf7cff0afd6dcd5bfa70a2d
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-01-05 15:57:25 +0100

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,5 @@
+age = int(input("Enter student's age:"))
+if age >= 10 and age <= 20:
+    print("Student is Allowed to enroll in the class")
+else:
+    print("The student is Not allowed to enroll in the class")
