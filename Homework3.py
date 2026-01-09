3883b98298a7694f00827744b1d22c6cc0234b99
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-01-09 16:39:40 +0100

    Update Homework3.py

--- Homework3.py
+++ Homework3.py
@@ -1,7 +1,10 @@
-a = 10
-b = 20
-c = 30
-
-a, b, c = c, a, b
-print("After swapping")
-print("a =", a,"b =", b,"c =", c,)
+a = 20
+b = 10
+c = 15
+a=a+b+c
+c=a-(b+c)
+a=a-(b+c)
+print("After Swapping")
+print("a=", a)
+print("b=", b)
+print("c=", c)
