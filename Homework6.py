d4cc980f69d1a4e6471241c9ef3c37f3e6e715b5
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-01-26 16:39:58 +0100

    Update Homework6.py

--- Homework6.py
+++ Homework6.py
@@ -1,10 +1,4 @@
 num = int(input("Enter a number"))
-if num == 0:
-    print(1)
-else:
-    count = 0
-    num = abs(num)
-    while num > 0:
-     count += 1
-     num //= 10  
-print(count)
+while num > 0:
+    print(num)
+    num = num-1
