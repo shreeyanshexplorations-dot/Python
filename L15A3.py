0dfcd3c53d447fe306ce7eb651c86ce01ef49fb7
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-02-02 16:55:18 +0100

    Update L15A3.py

--- L15A3.py
+++ L15A3.py
@@ -11,16 +11,16 @@ print("a. add")
 print("b. subtract")
 print("c. multiply")
 print("d. divide")
-choice = ("Please enter choice (a/ b/ c/ d):")
+choice = input("Please enter choice (a/ b/ c/ d):")
 num_1 = int (input("Please enter the first number:"))
 num_2 = int (input("Please enter the second number:"))
 if choice == 'a':
     print (num_1,"+",num_2, "=" ,add(num_1, num_2) )
-    if choice == 'b':
-     print (num_1,"-",num_2, "=",subtract(num_1, num_2) )
-     if choice == 'c':
+elif choice == 'b':
+    print (num_1,"-",num_2, "=",subtract(num_1, num_2) )
+elif choice == 'c':
       print (num_1,"*",num_2, "=",multiply(num_1, num_2) )
-    if choice == 'd':
+elif choice == 'd':
        print (num_1,"/",num_2, "=",divide(num_1, num_2) )
-    else:
+else:
        print("This is an invalid input")
