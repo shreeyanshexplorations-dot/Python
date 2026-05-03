806331a203fbb1e1cd4e684757dd560329f05957
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-05-03 16:42:11 +0200

    Add files via upload

---
+++ Homework19.py
@@ -0,0 +1,8 @@
+set1  = {'blue', 'green'}
+set2  = {'blue', 'yellow'}
+
+result_a = set1.symmetric_difference(set2)
+print("Symmetric Difference A:", result_a)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {1, 5, 6, 7, 8, 9}
