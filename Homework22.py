40644da1f5975f6a0ce5669abcbb557f34f32917
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-05-24 17:57:45 +0200

    Add files via upload

---
+++ Homework22.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+length = 8
+
+chars = string.ascii_letters + string.digits
+password = ''.join(random.choice(chars) for _ in range(length))
+
+print("Password:", password)
