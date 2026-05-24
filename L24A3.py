9de381520fd9dd931adfc81ddc3381175677af72
shreeyanshexplorations-dot <shreeyanshexplorations@gmail.com>
2026-05-24 19:28:30 +0200

    Add files via upload

---
+++ L24A3.py
@@ -0,0 +1,19 @@
+class Parrot:
+    species = "bird"
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+blu = Parrot("blu", 10)
+woo = Parrot("woo", 15)
+
+
+print("Blu is a {}".format(blu.species))
+print("Woo is also a {}".format(woo.species))
+
+
+print("{} is {} years old".format(blu.name, blu.age))
+print("{} is {} years old".format(woo.name, woo.age))
